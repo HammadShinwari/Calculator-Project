d01db9da08b4f29fb27f8bc0eaaf63df83bfba0e
HammadShinwari33 <hammadshinwari377@gmail.com>
2023-10-15 18:18:18 +0500

    Calculator-Project

---
+++ Calculator Project/script.js
@@ -0,0 +1,20 @@
+
+let result = document.getElementById("result");
+
+function display(number){
+    result.value += number;
+}
+
+function calculate(){
+    let final_number = result.value;
+    let final_result = eval(final_number);
+    result.value = final_result;
+}
+
+function clrs(){
+    result.value = "";
+}
+
+function dle(){
+ result.value = result.value.slice(0, -1)
+}
